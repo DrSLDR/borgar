8cd24280ffe6d22a9e97ebc6f5733fea08c24168
Jonas A. Hultén <jonas@sldr.se>
2021-01-14 16:54:54 +0100

    Added a stupid test test
    Baby's first mock. Good work.

--- borgar/borg_iface.py
+++ borgar/borg_iface.py
@@ -3,3 +3,9 @@
 The borgar-internal interface for communicating with (that is, mostly
 invoking) borg. We hide all that here for modularity and ease of use.
 """
+
+import subprocess
+
+
+def dummy():
+    subprocess.run(["touch", "yes"])
--- tests/test_borg_iface.py
+++ tests/test_borg_iface.py
@@ -6,8 +6,15 @@ before. I don't know how to write efficient tests. But, here we are.
 
 from borgar import borg_iface as BI
 
-from unittest.mock import MagicMock, Mock
+from unittest.mock import patch
 
 
 def test_dummy():
     assert 1 == 1
+
+
+@patch("subprocess.run")
+def test_dummy_2(mock):
+    print(mock)
+    BI.dummy()
+    mock.assert_called_once_with(["touch", "no"])
