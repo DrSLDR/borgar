aee88d00f7fca7dd03d329acd0eae3395a3a1293
Jonas A. Hultén <jonas@sldr.se>
2021-01-14 17:31:25 +0100

    Added existence function to test if borg is installed
    Also wrote baby's first "real" mock. Seems to work alright.

--- borgar/borg_iface.py
+++ borgar/borg_iface.py
@@ -7,5 +7,16 @@ invoking) borg. We hide all that here for modularity and ease of use.
 import subprocess
 
 
-def dummy():
-    subprocess.run(["touch", "yes"])
+def exists() -> bool:
+    """Checks that borg is installed and available on path
+
+    Returns:
+        bool: True if borg is available.
+    """
+    try:
+        cp = subprocess.run(
+            ["borg", "--version"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+        )
+        return cp.returncode == 0
+    except FileNotFoundError:
+        return False
--- tests/test_borg_iface.py
+++ tests/test_borg_iface.py
@@ -4,17 +4,29 @@ I'll be honest. I don't know what I'm doing here. I've never done mocking
 before. I don't know how to write efficient tests. But, here we are.
 """
 
+from unittest import mock
+import unittest
 from borgar import borg_iface as BI
 
-from unittest.mock import patch
-
-
-def test_dummy():
-    assert 1 == 1
+from unittest.mock import patch, MagicMock
+import subprocess
 
 
 @patch("subprocess.run")
-def test_dummy_2(mock):
-    print(mock)
-    BI.dummy()
-    mock.assert_called_once_with(["touch", "no"])
+def test_exists(mock_run: MagicMock):
+    # Test a good run
+    mock_run.return_value = subprocess.CompletedProcess(args=[], returncode=0)
+    assert BI.exists()
+    mock_run.assert_called()
+
+    # Test a failing run
+    mock_run.reset_mock(
+        return_value=subprocess.CompletedProcess(args=[], returncode=127)
+    )
+    assert not BI.exists()
+    mock_run.assert_called()
+
+    # Test with exception
+    mock_run.reset_mock(side_effect=FileNotFoundError("Je suis mock"))
+    assert not BI.exists()
+    mock_run.assert_called()
