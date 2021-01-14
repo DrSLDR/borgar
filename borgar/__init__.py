6bbc2f79be17dfc649ac97c5fd1d0d5fac2522d6
Jonas A. Hultén <jonas@sldr.se>
2021-01-14 15:26:23 +0100

    Added an init docstring
    For some reason I'm starting with documentation. Let's see if that ends
    up biting me in the ass.

---
+++ borgar/__init__.py
@@ -0,0 +1,7 @@
+"""Borg automation and offsite syncing system
+
+One man's brave attempt to handle and automate his own backuping as well as
+making sure it gets synced out to questionable offsite places.
+"""
+
+from . import borg_iface
